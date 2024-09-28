955c4c7a076ab9abd525f1fd93c474591f0e0874
Cabe Cullinan <ctcullinan@dmacc.edu>
2024-09-28 15:30:25 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/versioncontrol_cabecullinan/VersionControl_CabeCullinan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_cabecullinan;
+
+/**
+ *
+ * @author ccull
+ */
+public class VersionControl_CabeCullinan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! This is a basic Message to be Displayed!");
+    }
+}
